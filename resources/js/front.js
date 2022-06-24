586ba9613fe147b14f753c1ccd1b2848f4999c6c
Ennio-M <marrese.ennio@gmail.com>
2022-06-24 02:53:33 +0200

    costruzione componente con Vue

--- resources/js/front.js
+++ resources/js/front.js
@@ -19,7 +19,7 @@
  // const files = require.context('./', true, /\.vue$/i)
  // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
  
- Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+ //Vue.component('example-component', require('./components/ExampleComponent.vue').default);
  
  /**
   * Next, we will create a fresh Vue application instance and attach it to
@@ -27,7 +27,10 @@
   * or customize the JavaScript scaffolding to fit your unique needs.
   */
  
+import App from './views/App';
+
  const app = new Vue({
-     el: '#app',
+     el: '#root',
+     render: h => h(App)
  });
  
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guests.home');
-});
 
 Auth::routes();
 
@@ -28,3 +25,7 @@ Route::middleware('auth')
     ->group(function(){
         Route::get('/', 'HomeController@index')->name('home');
     });
+
+    Route::get("{any?}", function() {
+        return view("guest.home");
+    })->where("any", ".*");
